adf9f6b28c52ce2f9d686c168ce61df1304adeaf
Victor <Victor@VictorPC.iesb.br>
2015-05-04 21:56:35 -0300

    Quando o ultimo cliente sair, o servidor encerra automaticamente

--- src/br/iesb/servidor/app/service/ServidorService.java
+++ src/br/iesb/servidor/app/service/ServidorService.java
@@ -20,197 +20,209 @@ import br.iesb.app.bean.ChatMessage.Action;
 
 public class ServidorService implements Serializable {
 
-    /** Constante serialVersionUID. */
-    private static final long serialVersionUID = 67062559888939165L;
+	/** Constante serialVersionUID. */
+	private static final long serialVersionUID = 67062559888939165L;
 
-    /** Socket Servidor para abrir uma conexão. */
-    private ServerSocket serverSocket;
+	/** Socket Servidor para abrir uma conexão. */
+	private ServerSocket serverSocket;
 
-    /** Socket para receber a solicitação do cliente. */
-    private Socket socket;
-
-    /**
-     * Lista de usuários online no chat. <br>
-     * Vai servir p/ redistribuir a mensagem que o usuário digitar p/ todos os
-     * outros. </br></br> <code>String</code> Nome do usuário </br></br>
-     * <code>ObjectOutputStream</code> Tudo o que o usuário for digitar.
-     */
-    private Map<String, ObjectOutputStream> mapOnlines = new HashMap<String, ObjectOutputStream>();
-
-    public ServidorService() {
-	try {
-	    serverSocket = new ServerSocket(5555);
-	    while (true) {
-		System.out.println("Running Server...");
-		socket = serverSocket.accept();
-		new Thread(new ListenerSocket(socket)).start();
-	    }
-	} catch (IOException e) {
-	    JOptionPane.showMessageDialog(null, "Servidor já está rodando...");
-	}
-    }
-
-    /**
-     * Classe ListenerSocket. <br>
-     * </br> É o ouvinte do servidor.
-     */
-    private class ListenerSocket implements Runnable {
-
-	/** Executa o envio de mensagem p/ o(s) cliente(s). */
-	private ObjectOutputStream output;
-
-	/** Recebe a mensagem enviada pelo cliente. */
-	private ObjectInputStream input;
+	/** Socket para receber a solicitação do cliente. */
+	private Socket socket;
 
 	/**
-	 * Instancia um novo listener socket.
-	 *
-	 * @param socket
-	 *            socket da comunicação cliente-servidor estabelecida.
+	 * Lista de usuários online no chat. <br>
+	 * Vai servir p/ redistribuir a mensagem que o usuário digitar p/ todos os
+	 * outros. </br></br> <code>String</code> Nome do usuário </br></br>
+	 * <code>ObjectOutputStream</code> Tudo o que o usuário for digitar.
 	 */
-	public ListenerSocket(Socket socket) {
-	    try {
-		// input output agora pertencem exclusivamente do cliente que se
-		// conectou.
-		this.output = new ObjectOutputStream(socket.getOutputStream());
-		this.input = new ObjectInputStream(socket.getInputStream());
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    }
-	}
+	private Map<String, ObjectOutputStream> mapOnlines = new HashMap<String, ObjectOutputStream>();
 
-	@Override
-	public void run() {
-	    ChatMessage message = null;
-	    try {
-		while ((message = (ChatMessage) input.readObject()) != null) {
-		    switch (message.getAction()) {
-			case CONNECT:
-			    if (connect(message, output)) {
-				mapOnlines.put(message.getName(), output);
-				sendOnlines();
-			    } else
-				message = null;
-			    break;
-			case DISCONNECT:
-			    if (message != null) {
-				disconnect(message, output);
-				sendOnlines();
-			    }
-			    return;
-			case SEND_ONE:
-			    sendOne(message);
-			    break;
-			case SEND_ALL:
-			    sendAll(message);
-			    break;
-			default:
-			    break;
-		    }
-		}
-	    } catch (IOException e) {
-		ChatMessage cm = new ChatMessage();
-		if (message != null) {
-		    cm.setName(message.getName());
-		    disconnect(cm, output);
-		    sendOnlines();
-		    System.out.println(message.getName() + " deixou o chat!");
-		}
-	    } catch (ClassNotFoundException e) {
-		e.printStackTrace();
-	    }
-	}
-    }
-
-    /**
-     * Connect.
-     *
-     * @param message
-     *            mensagem recebida pelo servidor
-     * @param output
-     *            objeto que será enviado pelo servidor
-     * @return true, se a conexão foi estabelecida
-     */
-    private boolean connect(ChatMessage message, ObjectOutputStream output) {
-	if (mapOnlines.size() == 0) {
-	    message.setText("YES");
-	    send(message, output);
-	    return true;
-
-	}
-	if (mapOnlines.containsKey(message.getName())) {
-	    message.setText("NO");
-	    send(message, output);
-	    return false;
-	} else {
-	    message.setText("YES");
-	    send(message, output);
-	    return true;
-	}
-    }
-
-    private void disconnect(ChatMessage message, ObjectOutputStream output) {
-	mapOnlines.remove(message.getName());
-	message.setText(" deixou o chat!");
-	message.setAction(Action.SEND_ONE);
-	sendAll(message);
-	System.out.println("O usuário " + message.getName() + " saiu da sala");
-    }
-
-    private void sendOne(ChatMessage message) {
-	for (Map.Entry<String, ObjectOutputStream> kv : mapOnlines.entrySet()) {
-	    if (kv.getKey().equals(message.getNameReserved())) {
+	public ServidorService() {
 		try {
-		    kv.getValue().writeObject(message);
-		} catch (IOException ex) {
-		    Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
+			serverSocket = new ServerSocket(5555);
+			while (true) {
+				System.out.println("Running Server...");
+				socket = serverSocket.accept();
+				new Thread(new ListenerSocket(socket)).start();
+			}
+		} catch (IOException e) {
+			if(!(mapOnlines.isEmpty()))
+				JOptionPane.showMessageDialog(null, "Servidor já está rodando...");
+			//tratar quando estah vazia
 		}
-	    }
 	}
-    }
 
-    private void sendAll(ChatMessage message) {
-	for (Map.Entry<String, ObjectOutputStream> kv : mapOnlines.entrySet()) {
-	    if (!kv.getKey().equals(message.getName())) {
+	/**
+	 * Classe ListenerSocket. <br>
+	 * </br> É o ouvinte do servidor.
+	 */
+	private class ListenerSocket implements Runnable {
+
+		/** Executa o envio de mensagem p/ o(s) cliente(s). */
+		private ObjectOutputStream output;
+
+		/** Recebe a mensagem enviada pelo cliente. */
+		private ObjectInputStream input;
+
+		/**
+		 * Instancia um novo listener socket.
+		 *
+		 * @param socket
+		 *            socket da comunicação cliente-servidor estabelecida.
+		 */
+		public ListenerSocket(Socket socket) {
+			try {
+				// input output agora pertencem exclusivamente do cliente que se
+				// conectou.
+				this.output = new ObjectOutputStream(socket.getOutputStream());
+				this.input = new ObjectInputStream(socket.getInputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void run() {
+			ChatMessage message = null;
+			try {
+				while ((message = (ChatMessage) input.readObject()) != null) {
+					switch (message.getAction()) {
+					case CONNECT:
+						if (connect(message, output)) {
+							mapOnlines.put(message.getName(), output);
+							sendOnlines();
+						} else
+							message = null;
+						break;
+					case DISCONNECT:
+						if (message != null) {
+							disconnect(message, output);
+							if (!(mapOnlines.isEmpty()))
+								sendOnlines();
+						}
+						return;
+					case SEND_ONE:
+						sendOne(message);
+						break;
+					case SEND_ALL:
+						sendAll(message);
+						break;
+					default:
+						break;
+					}
+				}
+			} catch (IOException e) {
+				ChatMessage cm = new ChatMessage();
+				if (message != null) {
+					cm.setName(message.getName());
+					disconnect(cm, output);
+					sendOnlines();
+					System.out.println(message.getName() + " deixou o chat!");
+				}
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Connect.
+	 *
+	 * @param message
+	 *            mensagem recebida pelo servidor
+	 * @param output
+	 *            objeto que será enviado pelo servidor
+	 * @return true, se a conexão foi estabelecida
+	 */
+	private boolean connect(ChatMessage message, ObjectOutputStream output) {
+		if (mapOnlines.size() == 0) {
+			message.setText("YES");
+			send(message, output);
+			return true;
+
+		}
+		if (mapOnlines.containsKey(message.getName())) {
+			message.setText("NO");
+			send(message, output);
+			return false;
+		} else {
+			message.setText("YES");
+			send(message, output);
+			return true;
+		}
+	}
+
+	private void disconnect(ChatMessage message, ObjectOutputStream output) {
+		mapOnlines.remove(message.getName());
+		message.setText(" deixou o chat!");
 		message.setAction(Action.SEND_ONE);
-		try {
-		    kv.getValue().writeObject(message);
-		} catch (IOException ex) {
-		    Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
+		sendAll(message);
+		System.out.println("O usuário " + message.getName() + " saiu da sala");
+		if(mapOnlines.isEmpty()){
+			try {
+				socket.close();
+				serverSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-	    }
-	}
-    }
-
-    private void send(ChatMessage message, ObjectOutputStream output) {
-	try {
-	    output.writeObject(message);
-	} catch (IOException ex) {
-	    Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
-	}
-    }
-
-    /**
-     * Envia mensagens apenas aos usuário onlines selecionados.
-     */
-    private void sendOnlines() {
-	Set<String> setNames = new HashSet<String>();
-	for (Map.Entry<String, ObjectOutputStream> kv : mapOnlines.entrySet()) {
-	    setNames.add(kv.getKey());
 	}
 
-	ChatMessage message = new ChatMessage();
-	message.setAction(Action.USERS_ONLINE);
-	message.setSetOnlines(setNames);
-
-	for (Map.Entry<String, ObjectOutputStream> kv : mapOnlines.entrySet()) {
-	    message.setName(kv.getKey());
-	    try {
-		kv.getValue().writeObject(message);
-	    } catch (IOException ex) {
-		Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
-	    }
+	private void sendOne(ChatMessage message) {
+		for (Map.Entry<String, ObjectOutputStream> kv : mapOnlines.entrySet()) {
+			if (kv.getKey().equals(message.getNameReserved())) {
+				try {
+					kv.getValue().writeObject(message);
+				} catch (IOException ex) {
+					Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}
+		}
 	}
-    }
+
+	private void sendAll(ChatMessage message) {
+		for (Map.Entry<String, ObjectOutputStream> kv : mapOnlines.entrySet()) {
+			if (!kv.getKey().equals(message.getName())) {
+				message.setAction(Action.SEND_ONE);
+				try {
+					kv.getValue().writeObject(message);
+				} catch (IOException ex) {
+					Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}
+		}
+	}
+
+	private void send(ChatMessage message, ObjectOutputStream output) {
+		try {
+			output.writeObject(message);
+		} catch (IOException ex) {
+			Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	/**
+	 * Envia mensagens apenas aos usuário onlines selecionados.
+	 */
+	private void sendOnlines() {
+		Set<String> setNames = new HashSet<String>();
+		for (Map.Entry<String, ObjectOutputStream> kv : mapOnlines.entrySet()) {
+			setNames.add(kv.getKey());
+		}
+
+		ChatMessage message = new ChatMessage();
+		message.setAction(Action.USERS_ONLINE);
+		message.setSetOnlines(setNames);
+
+		for (Map.Entry<String, ObjectOutputStream> kv : mapOnlines.entrySet()) {
+			message.setName(kv.getKey());
+			try {
+				kv.getValue().writeObject(message);
+			} catch (IOException ex) {
+				Logger.getLogger(ServidorService.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+	}
 
 }
